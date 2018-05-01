a96201a741b11c48bb0d3e00997891566d224bc4
tailin <tailinwu2009@gmail.com>
2018-05-01 19:03:02 -0400

    Added requirements and .gitignore

---
+++ requirements.py
@@ -0,0 +1,8 @@
+numpy
+scikit-learn
+matplotlib
+
+# Dependencies:
+mpi4py # for linux, may require first do "sudo apt install libopenmpi-dev"
+opencv-python # for linux, may require "sudo apt-get install -y libxrender-dev"
+# sudo apt-get install python3-tk
